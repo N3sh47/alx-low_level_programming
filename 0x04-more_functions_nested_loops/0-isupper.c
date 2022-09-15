97faf6988cffb154fd117a64602ead55b07dc631
N3sh47 <munenejohn628@gmail.com>
2022-09-15 04:25:17 -0700

    initial commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 /**
- * check code
- * always return 0
+ * _isupper - function that checks for uppercase characters
+ * @c: int type number
+ * Return: 1 if uppercase, else 0
  */
 int _isupper(int c)
 {

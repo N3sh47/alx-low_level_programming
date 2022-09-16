cafbf04c2c0b4a25a1bb35e674a8cacf0b520bab
N3sh47 <munenejohn628@gmail.com>
2022-09-15 22:23:58 -0700

    initial commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ *positive_or_negative - prints whether n is positive or negative
+ *@n: int
+ *Return: 0
+ */
+
+void positive_or_negative(int n)
+{
+if (n > 0)
+
+{
+
+printf("%d is positive\n", n);
+
+}
+
+else if (n < 0)
+
+{
+
+printf("%d is negative\n", n);
+
+}
+
+else
+
+{
+
+printf("%d is zero\n", n);
+
+}
+
+return;
+
+}

2f8fcdc8a19daf5a47d8c8f322d73dae8ed9779d
N3sh47 <munenejohn628@gmail.com>
2022-09-22 03:14:55 -0700

    initial commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,24 @@
-#include "main.h"
-#include <stdio.h>
+#include holberton.h
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strncat - concatenate two strings
+ * @dest: string
+ * @src: string
+ * @n: number of elements to concatenate in
+ * Return: pointer to resulting 
  */
-int main(void)
-{
-	char s1[98] = "Hello ";
-	char s2[] = "World!\n";
-	char *ptr;
 
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strncat(s1, s2, 1);
-	printf("%s\n", s1);
-	printf("%s", s2);
-	printf("%s\n", ptr);
-	ptr = _strncat(s1, s2, 1024);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", ptr);
-	return (0);
+char *_strncat(char *dest, char *src, int n)
+{
+int i, c;
+
+for (i = 0; dest[i] != '\0'; i++)
+;
+
+for (c = 0; src[c] != '\0' && n > 0; c++, n--, i++)
+{
+dest[i] = src[c];
 }
+return (dest);
+}
+
